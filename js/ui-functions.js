34eaab58848edf73e0a3e130021b35b010d3ce8e
Bruno Mendes <brunomendes962@gmail.com>
2017-09-11 15:01:06 -0300

    Refatorando javascript

--- js/ui-functions.js
+++ js/ui-functions.js
@@ -1,38 +1,48 @@
-//evento de navegação do menu - scroll suave
-$(".menu-links li a, #btn-contact").on("click", function(event){
-  event.preventDefault();
-        
-     var target = $(this).attr("href");        
-     var target_pos = $(target).offset().top;
-    
-  //animação do scroll
-    $("html, body").animate({    
-      scrollTop: target_pos - 25,
-    }, 500);
-        
-});
+$(function(){
 
-//Evento click do menu hamburguer
-$("#btn-responsive-menu").on("click", function(event){
-  event.preventDefault();
-  $(".menu-links").toggleClass("menu-active"); 
-});
+  //---- FUNÇÕES SCROLL-----
+  //evento de navegação do menu - scroll suave
+  $(".menu-links li a, #btn-contact").on("click", (event) => {
+    event.preventDefault();
+          
+      let target = $(event.target).attr("href");        
+      let target_pos = $(target).offset().top;
+      
+    //animação do scroll
+      $("html, body").animate({    
+        scrollTop: target_pos - 25,
+      }, 500);
+          
+  });
 
-// colocando background no menu fixo dependendo da posição do scroll
-
-$(window).on('scroll', function(){
-  //posição do menu
-  var menu_pos = $(".menu-bg").offset().top;
-  if(menu_pos > 100){
-    $(".menu-bg").css({
-      backgroundColor: 'rgba(0,0,0, .9)'
-    })
-    
-  } else {
-    $(".menu-bg").css({
-      backgroundColor: 'transparent'
-    })
-    
-  }
+  // colocando background no menu dependendo da posição do scroll
+  $(window).on('scroll', () => {
+    //posição do menu
+    let menu_pos = $(".menu-bg").offset().top;
+    if(menu_pos > 100){
+      $(".menu-bg").css({
+        backgroundColor: 'rgba(17, 17, 17, .8)'
+      })
+      
+    } else {
+      $(".menu-bg").css({
+        backgroundColor: 'transparent'
+      })    
+    }  
+  });
   
+  //----- FUNÇÕES MENU ------
+  //Evento click do menu hamburguer
+  $("#btn-responsive-menu").on("click", (event)=> {
+    event.preventDefault();
+    $(".menu-links").toggleClass("menu-active"); 
+  });
+
+  //fechando menu-responsivo ao click de um link
+  $(".menu-links a").on("click", () => {
+    $(".menu-links").removeClass("menu-active"); 
+  });
+
 });
+
+
